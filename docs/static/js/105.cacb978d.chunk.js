f0da5cb8cd7c042b13ad47a37c15fc35f2f6e77d
AliShahbazzz <alishahbaz617@gmail.com>
2025-08-30 22:53:04 +0530

    Updated domain

--- docs/static/js/105.cacb978d.chunk.js
+++
@@ -1,9 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[105],{3533:(e,n,i)=>{i.r(n),i.d(n,{default:()=>t});i(5043);const a=(0,i(9).Ay)("section")`
-  position: relative;
-  padding: 1rem 0 8rem;
-
-  @media only screen and (max-width: 1024px) {
-    padding: 4rem 0 4rem;
-  }
-`;i(2502);var r=i(1577),s=i(579);const t=e=>{let{direction:n}=e;return(0,s.jsx)(a,{children:(0,s.jsxs)(r.FN,{autoPlay:!0,infiniteLoop:!0,showThumbs:!1,showStatus:!1,children:[(0,s.jsx)("div",{children:(0,s.jsx)("img",{src:"/landy-react-template/img/slider/banner1.png",alt:""})}),(0,s.jsx)("div",{children:(0,s.jsx)("img",{src:"/landy-react-template/img/slider/banner2.png",alt:""})}),(0,s.jsx)("div",{children:(0,s.jsx)("img",{src:"/landy-react-template/img/slider/banner3.png",alt:""})})]})})}}}]);
-//# sourceMappingURL=105.cacb978d.chunk.js.map
